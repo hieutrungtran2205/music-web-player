4f702a574a1afb9a7ff73c4824b29dc1afa4faf3
hieutrungtran2205 <trunghieu22052k@gmail.com>
2021-11-16 22:47:25 +0700

    not use json-server

--- app.js
+++ app.js
@@ -1,106 +1,134 @@
-var API_VPOP = "http://localhost:3000/v-pop";
-var API_USUK = "http://localhost:3000/us-uk";
-var API_KPOP = "http://localhost:3000/k-pop";
+var albumVPOP = [
+  {
+    album_name: "Muộn Rồi Mà Sao Còn",
+    singer: "Sơn Tùng M-TP",
+    thumbnail: "https://2sao.vietnamnetjsc.vn/images/2021/04/29/23/53/3.jpg",
+    id: 1,
+  },
+  {
+    album_name: "Lối Nhỏ",
+    singer: "Đen Vâu",
+    thumbnail:
+      "https://allvpop.com/wp-content/uploads/2019/11/27_300x300.jpg",
+    id: 2,
+  },
+  {
+    album_name: "Anh Ơi Ở Lại",
+    singer: "Chi Pu",
+    thumbnail:
+      "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/cover/7/1/6/a/716ad904bcbe6447f531f714a3148f5d.jpg",
+    id: 3,
+  },
+  {
+    album_name: "Trên Tình Bạn Dưới Tình Yêu",
+    singer: "Min",
+    thumbnail:
+      "https://419.vn/wp-content/uploads/2020/11/tren-tinh-ban-duoi-tinh-yeu-min.jpg",
+    id: 4,
+  },
+  {
+    album_name: "Em Đã Thương Người Ta Hơn Anh",
+    singer: "Noo Phước Thịnh",
+    thumbnail:
+      "https://static2.yan.vn/YanNews/202009/202009091152579578-9c8a1dfa-4492-4215-9b85-dadb34361a80.png",
+    id: 5,
+  },
+  {
+    album_name: "Có Hẹn Với Thanh Xuân",
+    singer: "MONSTAR",
+    thumbnail:
+      "https://lyricvn.com/wp-content/uploads/2021/07/e23ff2faaa64eebfc57e0acde247f0db.jpg",
+    id: 6,
+  },
+];
 
-const $ = document.querySelector.bind(document);
-
-function start() {
-  getVPOP(renderVPOP);
-  getUSUK(renderUSUK);
-  getKPOP(renderKPOP);
-}
-
-start();
-
-function getVPOP(callback) {
-  fetch(API_VPOP)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(callback)
-}
-
-function getUSUK(callback) {
-  fetch(API_USUK)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(callback)
-}
-
-function getKPOP(callback) {
-  fetch(API_KPOP)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(callback)
-}
-
-function renderVPOP(albums) {
-  var listVPOP = $(".v-pop");
-  console.log(albums)
-  var htmls = albums.map(function (album) {
-    return `
-          <li class="album">
-            <a href="play.html">
-              <div
-                class="thumb"
-                style="background-image: url('${album.thumbnail}')"
-              ></div>
-              <div class="info">
-                <span class="song">${album.album_name}</span>
-                <p class="singer">${album.singer}</p>
-              </div>
-            </a>
-          </li>
-          `
-  });
-  listVPOP.innerHTML = htmls.join('');
-}
-
-function renderUSUK(albums) {
-  var listUSUK = $(".us-uk");
-  console.log(albums)
-  var htmls = albums.map(function (album) {
-    return `
-          <li class="album">
-            <a href="play.html">
-              <div
-                class="thumb"
-                style="background-image: url('${album.thumbnail}')"
-              ></div>
-              <div class="info">
-                <span class="song">${album.album_name}</span>
-                <p class="singer">${album.singer}</p>
-              </div>
-            </a>
-          </li>
-          `
-  });
-  listUSUK.innerHTML = htmls.join('');
-}
-
-function renderKPOP(albums) {
-  var listKPOP = $(".k-pop");
-  console.log(albums)
-  var htmls = albums.map(function (album) {
-    return `
-          <li class="album">
-            <a href="play.html">
-              <div
-                class="thumb"
-                style="background-image: url('${album.thumbnail}')"
-              ></div>
-              <div class="info">
-                <span class="song">${album.album_name}</span>
-                <p class="singer">${album.singer}</p>
-              </div>
-            </a>
-          </li>
-          `
-  });
-  listKPOP.innerHTML = htmls.join('');
-}
+var albumUSUK = [
+  {
+    album_name: "Real Friend",
+    singer: "Camila Camello",
+    thumbnail:
+      "https://c-cl.cdn.smule.com/rs-s77/arr/a9/79/4d2a11db-d2e6-48c7-b9bf-9213d9c79cd1_1024.jpg",
+    id: 1,
+  },
+  {
+    album_name: "We Don't Talk Anymore",
+    singer: "Charlie Puth",
+    thumbnail:
+      "https://avatar-ex-swe.nixcdn.com/song/2017/09/27/a/9/e/f/1506480482657_640.jpg",
+    id: 2,
+  },
+  {
+    album_name: "At My Worst",
+    singer: "Pink Sweat$",
+    thumbnail:
+      "https://i1.sndcdn.com/artworks-dy2MNuVkrFcotrY7-nwiJRw-t500x500.jpg",
+    id: 3,
+  },
+  {
+    album_name: "I Love You 3000",
+    singer: "Stephanie Poetri",
+    thumbnail:
+      "https://avatar-ex-swe.nixcdn.com/song/2019/07/15/6/c/9/7/1563161317516_640.jpg",
+    id: 4,
+  },
+  {
+    album_name: "Let Me Down Slowly",
+    singer: "Alec Benjamin",
+    thumbnail:
+      "https://upload.wikimedia.org/wikipedia/vi/thumb/6/6a/Alec_Benjamin_-_Let_Me_Down_Slowly_single_artwork.jpg/220px-Alec_Benjamin_-_Let_Me_Down_Slowly_single_artwork.jpg",
+    id: 5,
+  },
+  {
+    album_name: "Play Date",
+    singer: "Melanie Martinez",
+    thumbnail:
+      "https://avatar-ex-swe.nixcdn.com/song/2020/07/24/f/6/5/1/1595559874883_640.jpg",
+    id: 6,
+  },
+];
+var albumKPOP = [
+  {
+    album_name: "How You Like That",
+    singer: "BLACKPINK",
+    thumbnail:
+      "https://kenh14cdn.com/2020/6/23/hylt-poster-1592784840060717374470-15929037318401929119192.jpeg",
+    id: 1,
+  },
+  {
+    album_name: "Made",
+    singer: "BIGBANG",
+    thumbnail:
+      "https://kenh14cdn.com/thumb_w/600/b5PLjxCEBcxai8yOBRqu8rXVeKDjdM/Image/2015/04/2/150417musicBIGBANG1-d63ba.jpg",
+    id: 2,
+  },
+  {
+    album_name: "BBoom BBoom",
+    singer: "MOMOLAND",
+    thumbnail: "https://data.chiasenhac.com/data/cover/82/81321.jpg",
+    id: 3,
+  },
+  {
+    album_name: "Day by Day",
+    singer: "T-ara",
+    thumbnail:
+      "https://avatar-ex-swe.nixcdn.com/song/2017/11/15/9/a/b/f/1510714223345_640.jpg",
+    id: 4,
+  },
+  {
+    album_name: "XOXO",
+    singer: "EXO",
+    thumbnail:
+      "http://images6.fanpop.com/image/photos/34400000/EXO-XOXO-teaser-pics-exo-m-34492821-600-400.jpg",
+    id: 5,
+  },
+  {
+    album_name: "Perfect World",
+    singer: "TWICE",
+    thumbnail:
+      "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/9ccdfced-e839-42d5-a415-5966da9a4e31/deo10j0-19e73bd4-16dd-4649-805e-2028935b96f2.jpg/v1/fill/w_1920,h_1920,q_75,strp/twice___perfect_world_by_diyeah9tee4_deo10j0-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTkyMCIsInBhdGgiOiJcL2ZcLzljY2RmY2VkLWU4MzktNDJkNS1hNDE1LTU5NjZkYTlhNGUzMVwvZGVvMTBqMC0xOWU3M2JkNC0xNmRkLTQ2NDktODA1ZS0yMDI4OTM1Yjk2ZjIuanBnIiwid2lkdGgiOiI8PTE5MjAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.RZxd4bS2YLtT9Y0JaULMT9eUF0-Gq85GmoKWI6uaPkA",
+    id: 6,
+  },
+];
 
 function goBack() {
   window.history.back();
@@ -114,6 +142,110 @@ function logClick() {
   const log = document.querySelector(".log");
   log.classList.toggle("hidden");
 }
+
+// var API_VPOP = "http://localhost:3000/v-pop";
+// var API_USUK = "http://localhost:3000/us-uk";
+// var API_KPOP = "http://localhost:3000/k-pop";
+
+// const $ = document.querySelector.bind(document);
+
+// function start() {
+//   getVPOP(renderVPOP);
+//   getUSUK(renderUSUK);
+//   getKPOP(renderKPOP);
+// }
+
+// start();
+
+// function getVPOP(callback) {
+//   fetch(API_VPOP)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(callback)
+// }
+
+// function getUSUK(callback) {
+//   fetch(API_USUK)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(callback)
+// }
+
+// function getKPOP(callback) {
+//   fetch(API_KPOP)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(callback)
+// }
+
+// function renderVPOP(albums) {
+//   var listVPOP = $(".v-pop");
+//   console.log(albums)
+//   var htmls = albums.map(function (album) {
+//     return `
+//           <li class="album">
+//             <a href="play.html">
+//               <div
+//                 class="thumb"
+//                 style="background-image: url('${album.thumbnail}')"
+//               ></div>
+//               <div class="info">
+//                 <span class="song">${album.album_name}</span>
+//                 <p class="singer">${album.singer}</p>
+//               </div>
+//             </a>
+//           </li>
+//           `
+//   });
+//   listVPOP.innerHTML = htmls.join('');
+// }
+
+// function renderUSUK(albums) {
+//   var listUSUK = $(".us-uk");
+//   console.log(albums)
+//   var htmls = albums.map(function (album) {
+//     return `
+//           <li class="album">
+//             <a href="play.html">
+//               <div
+//                 class="thumb"
+//                 style="background-image: url('${album.thumbnail}')"
+//               ></div>
+//               <div class="info">
+//                 <span class="song">${album.album_name}</span>
+//                 <p class="singer">${album.singer}</p>
+//               </div>
+//             </a>
+//           </li>
+//           `
+//   });
+//   listUSUK.innerHTML = htmls.join('');
+// }
+
+// function renderKPOP(albums) {
+//   var listKPOP = $(".k-pop");
+//   console.log(albums)
+//   var htmls = albums.map(function (album) {
+//     return `
+//           <li class="album">
+//             <a href="play.html">
+//               <div
+//                 class="thumb"
+//                 style="background-image: url('${album.thumbnail}')"
+//               ></div>
+//               <div class="info">
+//                 <span class="song">${album.album_name}</span>
+//                 <p class="singer">${album.singer}</p>
+//               </div>
+//             </a>
+//           </li>
+//           `
+//   });
+//   listKPOP.innerHTML = htmls.join('');
+// }
 
 
 
